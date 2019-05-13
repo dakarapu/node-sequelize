0efd1e04762406df132f4ef41df90c598c5fb779
Ravikanth Dakarapu <ravikanth.dakarapu@gmail.com>
2019-05-13 16:16:47 +1000

    first commit

---
+++ bin/dev.js
@@ -0,0 +1,5 @@
+require("@babel/register");
+require("@babel/core");
+require("@babel/polyfill"); // If you use async and await
+// This is where our main application index path is
+require("../index");
---
+++ src/app.js
@@ -0,0 +1,5 @@
+const app = function() {
+  return "This is App function...";
+};
+
+export default app;
